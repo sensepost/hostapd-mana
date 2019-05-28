86f046e5c7b5cdc5511cce950ffae8381243fbf5
Samy Baiwir <no0be@pm.me>
2019-05-28 13:33:07 +0200

    Pull OpenSSL submodule before trying to compile

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "[*] Downloading OpenSSL..."
+git submodule init
+git submodule update
+
 echo "[*] Building OpenSSL..."
 cd openssl
 ./config --prefix=$(pwd)/local enable-ssl2 enable-ssl3 enable-ssl3-method enable-des enable-rc4 enable-weak-ssl-ciphers no-shared
