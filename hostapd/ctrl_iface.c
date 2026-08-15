bcd1536226f14f7061cc91f0bb753a5d838e05af
Dominic White <singe-github@singe.za.net>
2026-08-15 15:48:27 +0200

    mana: add control interface commands
    (cherry picked from commit f5d77f49ab8dc20018d6e18636b760d83e62399c)

--- hostapd/ctrl_iface.c
+++ hostapd/ctrl_iface.c
@@ -71,7 +71,7 @@
 #include "fst/fst_ctrl_iface.h"
 #include "config_file.h"
 #include "ctrl_iface.h"
-
+#include "mana/ctrl_iface.h"
 
 #define HOSTAPD_CLI_DUP_VALUE_MAX_LEN 256
 
@@ -136,7 +136,6 @@ static int hostapd_ctrl_iface_new_sta(struct hostapd_data *hapd,
 	hostapd_new_assoc_sta(hapd, sta, 0);
 	return 0;
 }
-
 
 #ifdef NEED_AP_MLME
 static int hostapd_ctrl_iface_sa_query(struct hostapd_data *hapd,
@@ -231,7 +230,6 @@ static int hostapd_ctrl_iface_wps_check_pin(
 
 	return ret;
 }
-
 
 #ifdef CONFIG_WPS_NFC
 static int hostapd_ctrl_iface_wps_nfc_tag_read(struct hostapd_data *hapd,
@@ -4068,7 +4066,7 @@ static int hostapd_ctrl_iface_receive_process(struct hostapd_data *hapd,
 					      struct sockaddr_storage *from,
 					      socklen_t fromlen)
 {
-	int reply_len, res;
+	int reply_len, res, mana_res;
 
 	os_memcpy(reply, "OK\n", 3);
 	reply_len = 3;
@@ -4696,6 +4694,10 @@ static int hostapd_ctrl_iface_receive_process(struct hostapd_data *hapd,
 		if (hostapd_ctrl_afc_send_request(hapd, buf + 16) < 0)
 			reply_len = -1;
 #endif /* CONFIG_AFC */
+	} else if ((mana_res = mana_ctrl_iface_process(
+			    hapd, buf, reply, reply_size, &reply_len)) != 0) {
+		if (mana_res < 0)
+			reply_len = -1;
 	} else {
 		os_memcpy(reply, "UNKNOWN COMMAND\n", 16);
 		reply_len = 16;
